53dcb04546a8708dc16b73b1e3440bfec7236202
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-20 14:27:56 -0500

    Add hero mini detail in HeroesComponent

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.heroService.getHeroes().then((heroes) => this.heroes = heroes);
@@ -19,6 +24,10 @@ export class HeroesComponent implements OnInit {
 
   onHeroSelected(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  gotoDetail() {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
 
