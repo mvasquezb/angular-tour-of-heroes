7921607528d5c77643b8ac43702a799efbe053ca
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-20 14:50:00 -0500

    Add more styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroService } from './hero.service';
