609d8f4f7f7028d028257af9f343cebee877285d
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-21 04:36:35 -0500

    Add hero creation

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -29,5 +29,17 @@ export class HeroesComponent implements OnInit {
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+
+  add(name: string) {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.create(name)
+      .then((hero) => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      })
+  }
 }
 
