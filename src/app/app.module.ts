9dbb39a0d40f48a0eb0090909cbb15fe4e76b6f3
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-21 00:24:00 -0500

    Add HttpModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroService } from './hero.service';
@@ -19,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule,
   ],
   providers: [HeroService],
