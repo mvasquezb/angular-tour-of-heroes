0643238c531c7003e30a88ada4f7cfffcfd248e0
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-21 04:02:32 -0500

    Fetch from web api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) {}
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then((response) => response.json() as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.log(error);
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: number): Promise<Hero> {
